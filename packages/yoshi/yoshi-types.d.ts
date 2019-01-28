20ab1f5fc144afd1221fa10e37b4f76bfb48154a
Saar Kuriel <saark@wix.com>
2019-01-28 10:08:06 +0200

    added yoshi-types WIP

---
+++ packages/yoshi/yoshi-types.d.ts
@@ -0,0 +1,80 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '*.md' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.html' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+  const src: string;
+  export default src;
+}
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.less' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
+declare module '*.graphql' {
+  import {DocumentNode} from 'graphql';
+
+  const value: DocumentNode;
+  export = value;
+}
+
