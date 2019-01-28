9d34490d43be87e6d1f65cbfd8b4e014e18075ff
Saar Kuriel <saark@wix.com>
2019-01-28 13:17:48 +0200

    use yoshi-types in all project templates

--- packages/create-yoshi-app/templates/client-typescript/src/external-types.d.ts
+++ packages/create-yoshi-app/templates/client-typescript/src/external-types.d.ts
@@ -1,6 +1,3 @@
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
 interface Window {
   __STATICS_BASE_URL__: string;
   __LOCALE__: string;
--- packages/yoshi/yoshi-types.d.ts
+++ packages/yoshi/yoshi-types.d.ts
@@ -48,27 +48,22 @@ declare module '*.svg' {
 
 declare module '*.css' {
   const classes: { [key: string]: string };
-  export default classes;
+  export = classes;
 }
 
 declare module '*.scss' {
   const classes: { [key: string]: string };
-  export default classes;
+  export = classes;
 }
 
 declare module '*.sass' {
   const classes: { [key: string]: string };
-  export default classes;
+  export = classes;
 }
 
 declare module '*.less' {
   const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module "*.json" {
-  const value: any;
-  export default value;
+  export = classes;
 }
 
 declare module '*.graphql' {
